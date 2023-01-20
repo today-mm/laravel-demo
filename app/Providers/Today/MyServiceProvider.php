b72cfba736cd011bb06253d11997c2e7177e1dc6
ChristianP <1445466-poohspear@users.noreply.gitlab.com>
2023-01-20 10:53:31 +0630

    Add Laravel Admin Scafolding

--- app/Providers/Today/MyServiceProvider.php
+++ app/Providers/Today/MyServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Providers;
+namespace App\Providers\Today;
 
 use Illuminate\Support\ServiceProvider;
 
--- database/factories/Today/FooterFactory.php
+++ database/factories/Today/FooterFactory.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Database\Factories;
+namespace Database\Factories\Today;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
